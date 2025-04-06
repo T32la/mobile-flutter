ee216d08260f5946936f713f1b837daff669b591
Victoriano <victorianodev@gmail.com>
2025-04-06 16:50:18 -0600

    build(navbar) creat navBar

--- lib/views/Home.dart
+++ lib/views/Home.dart
@@ -1,3 +1,4 @@
+import 'package:apicat/views/Randomcat.dart';
 import 'package:flutter/material.dart';
 
 import 'package:transparent_image/transparent_image.dart';
@@ -17,52 +18,58 @@ class _MainScreenState extends State<MainScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("Home Cat's")),
-      // body: Center(
-      //   child: FadeInImage.memoryNetwork(
-      //     placeholder: kTransparentImage,
-      //     image:
-      //         // "https://www.catsluvus.com/wp-content/uploads/2023/04/Baby-Cats-Cute-and-Funny-Cat-Videos-Awesome.jpg",
-      //         'https://images.unsplash.com/photo-1533738363-b7f9aef128ce?q=80&w=1935&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-      //     width: 300,
-      //   ),
-      // ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment:
-              MainAxisAlignment.center, // Centra los elementos verticalmente
-          crossAxisAlignment:
-              CrossAxisAlignment.center, // Centra los elementos horizontalmente
-          children: [
-            FadeInImage.memoryNetwork(
-              placeholder: kTransparentImage,
-              image:
-                  'https://images.unsplash.com/photo-1533738363-b7f9aef128ce?q=80&w=1935&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-              width: 300,
-            ),
-            const SizedBox(height: 20), // Espaciador entre la imagen y el texto
-            const Text(
-              '¡Mira qué lindo gatito! 🐱',
-              style: TextStyle(
-                fontSize: 18,
-                fontWeight: FontWeight.bold,
+        appBar: AppBar(title: Text("Home Cat's")),
+        // body: Center(
+        //   child: FadeInImage.memoryNetwork(
+        //     placeholder: kTransparentImage,
+        //     image:
+        //         // "https://www.catsluvus.com/wp-content/uploads/2023/04/Baby-Cats-Cute-and-Funny-Cat-Videos-Awesome.jpg",
+        //         'https://images.unsplash.com/photo-1533738363-b7f9aef128ce?q=80&w=1935&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+        //     width: 300,
+        //   ),
+        // ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment:
+                MainAxisAlignment.center, // Centra los elementos verticalmente
+            crossAxisAlignment: CrossAxisAlignment
+                .center, // Centra los elementos horizontalmente
+            children: [
+              FadeInImage.memoryNetwork(
+                placeholder: kTransparentImage,
+                image:
+                    'https://images.unsplash.com/photo-1533738363-b7f9aef128ce?q=80&w=1935&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+                width: 300,
               ),
-            ),
-          ],
+              const SizedBox(
+                  height: 20), // Espaciador entre la imagen y el texto
+              const Text(
+                '¡Mira qué lindo gatito! 🐱',
+                style: TextStyle(
+                  fontSize: 18,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ],
+          ),
         ),
-      ),
-      // bottomNavigationBar: BottomNavigationBar(
-      //     items: [
-      //       BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
-      //       BottomNavigationBarItem(
-      //           icon: Icon(Icons.search), label: "Search"),
-      //     ],
-      //     onTap: (index) {
-      //       if (index == 0) {
-      //         Navigator.push(
-      //             context, MaterialPageRoute(builder: (context) => V2()));
-      //       }
-      //     })
-    );
+        bottomNavigationBar: BottomNavigationBar(
+            items: [
+              BottomNavigationBarItem(
+                  icon: Icon(Icons.castle_outlined), label: "Home Cat's"),
+              BottomNavigationBarItem(
+                  icon: Icon(Icons.rocket_launch_outlined),
+                  label: "Random Cat's"),
+            ],
+            onTap: (index) {
+              if (index == 0) {
+                Navigator.push(
+                    context, MaterialPageRoute(builder: (context) => V2()));
+              }
+              if (index == 1) {
+                Navigator.push(context,
+                    MaterialPageRoute(builder: (context) => Randomcat()));
+              }
+            }));
   }
 }
---
+++ lib/views/Randomcat.dart
@@ -0,0 +1,14 @@
+/////VISTA 3
+import 'package:flutter/material.dart';
+
+class Randomcat extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Michis 2')),
+      body: Center(
+        child: Text("Randoms cats's"),
+      ),
+    );
+  }
+}
